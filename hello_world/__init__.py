46a34e54b314a8c84115caaec8776ae9dad7a554
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-04-03 12:35:59 +0200

    add create dict

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,4 +1,4 @@
 print("Initializing hello world module")
-from .some_funcs import say_hello, repeat_string
+from .some_funcs import say_hello, repeat_string, create_dict
 
-__all__ = ["say_hello", "repeat_string"]
+__all__ = ["say_hello", "repeat_string", "create_dict"]
--- hello_world/some_funcs.py
+++ hello_world/some_funcs.py
@@ -3,3 +3,6 @@ def say_hello(x):
 
 def repeat_string(x, n):
     return x * n
+
+def create_dict(n=10):
+    return dict(zip(range(n), range(n)))
